b12cb5236bfd7dd9b2fafe829f07088ac9694da7
Giovanna Neves Rodrigues <giovanna.neves@sou.inteli.edu.br>
2025-09-05 00:03:43 -0300

    feat: conclusão parcial do projeto
    OBS: Add comentários + revisão

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -16,21 +16,34 @@ df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
 
+    # 5
+    df_cat = pd.melt(
+    df,
+    id_vars=['cardio'],
+    value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    )
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.value_counts().reset_index(name='total')
+    df_cat = df_cat.rename(columns={'variable': 'feature'})
 
     # 7
+    
+    g = sns.catplot(
+    data=df_cat,
+    x="feature", y="total",
+    hue="value", col="cardio",
+    kind="bar",
+    order=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke']
+    )
 
 
+    g.set_axis_labels("variable", "total")
+    g.set_titles("cardio = {col_name}")
 
     # 8
-    fig = None
-
+    fig = g.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -39,23 +52,44 @@ def draw_cat_plot():
 
 # 10
 def draw_heat_map():
+
     # 11
-    df_heat = None
+    df_heat = df.copy()
+
+    df_heat = df_heat[
+        (df_heat['ap_lo'] <= df_heat['ap_hi']) &
+        (df_heat['height'] >= df_heat['height'].quantile(0.025)) &
+        (df_heat['height'] <= df_heat['height'].quantile(0.975)) &
+        (df_heat['weight'] >= df_heat['weight'].quantile(0.025)) &
+        (df_heat['weight'] <= df_heat['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr(numeric_only=True)
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     # 15
 
+    sns.heatmap(
+    corr,
+    mask=mask,        
+    annot=True,     
+    fmt=".1f",       
+    center=0,        
+    cmap="RdBu",    
+    square=True,     
+    cbar=True,        
+    linewidths=.5,   
+    ax=ax
+    )
 
+    ax.set_title("Mapa de calor - Correlação (triângulo inferior)")
+    plt.tight_layout()
 
     # 16
     fig.savefig('heatmap.png')
